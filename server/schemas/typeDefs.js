96e40e88f99bb6207f7b62666062e015ccfdd33b
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-27 17:37:16 -0500

    fixed a mispelling of string in saveBook

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -28,9 +28,9 @@ const typeDefs = gql`
     }
 
     type Mutation {
-        login(email: String!, password: String!): Auth
+        loginUser(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!) Auth
-        saveBook(authors:[Author], description: Sring!, title: String!, bookId: String!, image: String!, link: String!) User
+        saveBook(authors:[Author], description: String!, title: String!, bookId: String!, image: String!, link: String!) User
         removeBook(bookId: String!) User
 
     }
