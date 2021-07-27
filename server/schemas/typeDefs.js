ffb80027b90a1d80ef7c3ee137c4ca01b2668ea4
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-27 17:11:07 -0500

    started to add schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,39 @@
+const { gql } = require('apollo-server-express')
+
+const typeDefs = gql`
+    type Book {
+        bookId: String!
+        authors: [Author]
+        description: String!
+        title: String! 
+        img: String!
+        link: String!
+    }
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Query {
+        me: [User]
+    }
+
+    type Auth {
+        token: String!
+        user: [User]
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!) Auth
+        saveBook(authors:[Author], description: Sring!, title: String!, bookId: String!, image: String!, link: String!) User
+        removeBook(bookId: String!) User
+
+    }
+`
+
+module.exports = typeDefs
