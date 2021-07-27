3e1164e27a66c500e97219a7e373686e9eb691ca
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-27 17:38:06 -0500

    added addUser saveBook and removeBook

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,10 +1,43 @@
 import { gql } from '@apollo/client'
 
-export const LOGIN = gql`
-    mutation login(email: Sring!, password: Sring!) {
-        login(email: $email, password: $password){
+export const LOGIN_USER = gql`
+    mutation loginUser(email: Sring!, password: Sring!) {
+        loginUser(email: $email, password: $password){
            token
            user 
+        }
+    }
+`
+
+export const ADD_USER = gql`
+    mutation addUser(username: String!, email: String!, password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user
+        }
+    }
+`
+
+export const SAVE_BOOK = gql`
+    mutation saveBook(authors:[Author], description: String!, title: String!, bookId: String!, image: String!, link: String!) {
+        saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks
+        }
+    }
+`
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook(bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks
         }
     }
 `
