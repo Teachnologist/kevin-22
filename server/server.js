2e03f5d50b5c1c0b6e69c039dd8c325844829cc2
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-29 13:07:21 -0500

    added use routes

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
 const express = require('express')
-const path = require('path')
-const db = require('./config/connection')
-const routes = require('./routes')
 const { ApolloServer } = require('apollo-server-express')
-const { typeDefs, resolvers } = require('./schemas')
+const path = require('path')
 
-const app = express()
+const routes = require('./routes')
+
+const { typeDefs, resolvers } = require('./schemas')
+const { authMiddleware } = require('./utils/auth')
+
+const db = require('./config/connection')
+
 const PORT = process.env.PORT || 3001
+const app = express()
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 })
 
 server.applyMiddleware({ app })
@@ -22,6 +27,8 @@ app.use(express.json())
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')))
 }
+
+app.use(routes)
 
 // app.use(routes)
 app.get('*', (req, res) => {
