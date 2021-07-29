4332f59899ac067eed6847f9cf0ba23675c73f45
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-29 18:20:51 -0500

    removed routs

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 const { ApolloServer } = require('apollo-server-express')
-const routes = require('./routes')
 
 const { typeDefs, resolvers } = require('./schemas')
 const { authMiddleware } = require('./utils/auth')
@@ -19,7 +18,7 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app })
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 // if we're in production, serve client/build as static assets
@@ -27,11 +26,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')))
 }
 
-app.use(routes)
-
-// app.use(routes)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'))
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
 })
 
 db.once('open', () => {
