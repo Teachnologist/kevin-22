097a2caf602b0779437a707ec9c79e896e11016d
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-29 09:07:04 -0500

    added switched from type to input SaveBookIn

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,36 +2,48 @@ const { gql } = require('apollo-server-express')
 
 const typeDefs = gql`
     type Book {
-        bookId: String!
-        authors: [Author]
-        description: String!
-        title: String! 
-        img: String!
-        link: String!
+        _id: String
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
     }
 
     type User {
-        _id: ID!
+        _id: ID
         username: String!
         email: String!
+        password: String!
         bookCount: Int
         savedBooks: [Book]
     }
 
+    input SaveBookIn {
+        authors: [String]
+        description: String
+        title: String
+        bookId: String
+        image: String
+        link: String
+
+    }
+
     type Query {
-        me: [User]
+        me: User
     }
 
     type Auth {
         token: String!
-        user: [User]
+        user: User
     }
 
     type Mutation {
         loginUser(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!) Auth
-        saveBook(authors:[Author], description: String!, title: String!, bookId: String!, image: String!, link: String!) User
-        removeBook(bookId: String!) User
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(book: SaveBookIn): User
+        removeBook(bookId: String!): User
 
     }
 `
